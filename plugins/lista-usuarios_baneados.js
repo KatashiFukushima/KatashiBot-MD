bffebae3512f94ad21c7b0370ab348d8b5745d94
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-09-16 18:28:36 -0500

    𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂

--- plugins/lista-usuarios_baneados.js
+++ plugins/lista-usuarios_baneados.js
@@ -6,7 +6,7 @@ let caption = `
 │
 │ *${i + 1}.* ${conn.getName(jid) == undefined ? 'Sin Usuarios Baneados' : conn.getName(jid)}
 │ ${isOwner ? '@' + jid.split`@`[0] : jid}\n│ - - - - - - - - -`.trim()).join('\n') : ''}
-│ *Estos usuarios no puedes Usar a GataBot*
+│ *Estos usuarios no pueden usar a KatashiBot*
 *╰•·–––––––––––––––––––·•*`.trim()
 
 await conn.reply(m.chat, caption, m, { mentions: await conn.parseMention(caption) })}
