7ca5f1ce5597ba55a737915e0117c1b2a3087186
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-09-16 17:42:36 -0500

    𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂

--- plugins/propietario(a)-update.js
+++ plugins/propietario(a)-update.js
@@ -7,9 +7,8 @@ conn.reply(m.chat, stdout.toString(), m)}
 //} catch {
 var update = execSync('git remote set-url origin https://github.com/KatashiFukushima/KatashiBot-MD.git && git pull')
 await m.reply(update.toString())
-} catch {
-await m.reply(`${fg}`) 
-}}
+}
+  
 handler.help = ['update']
 handler.tags = ['owner']
 handler.command = /^update|actualizar$/i
