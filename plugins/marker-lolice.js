f3f534ecde2ecf07ae88ced29ea1087c70bfc300
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-09-17 10:08:22 -0500

    𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂

--- plugins/marker-lolice.js
+++ plugins/marker-lolice.js
@@ -1,12 +1,10 @@
-let handler = async (m, { conn, usedprefix }) => {
-let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-conn.sendMessage(m.chat, {image:{ url: global.API('https://some-random-api.ml', '/canvas/lolice', { 
-avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')})}, caption: '*🚔🚨 LOLICONES COMO TU SOLO PERTENECEN A LA CARCEL 🚨🚔*', wm},
-// [['YO AMO LAS LOLIS 💓', `/loli`]], 
-m)
-}
-
-handler.help = ['lolice']
-handler.tags = ['maker']
-handler.command = /^(lolice)$/i
-export default handler
+const handler = async (m, {conn}) => {
+  const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/lolice', {
+    avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+  }), 'error.png', '*LLAMEN A LA POLICÍA!! 😱*', m);
+};
+handler.help = ['lolice'];
+handler.tags = ['maker'];
+handler.command = /^(lolice)$/i;
+export default handler;
