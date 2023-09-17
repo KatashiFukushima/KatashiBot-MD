9f4b67e821ba7d7fdcda35a9943a37bb7bf79a51
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-09-17 09:47:14 -0500

    𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -1,10 +1,12 @@
-export function before(m) {
+let handler = m => m 
+handler.before = async function (m, { text, args, usedPrefix, command, conn } ) {
 let user = global.db.data.users[m.sender]
 if (user.afk > -1) {
-m.reply(`
-${lenguajeGB['smsAfkM1']()}${user.afkReason ? ` ${lenguajeGB['smsAfkM2']()} ` + user.afkReason : ''}
-  
-${lenguajeGB['smsAfkM3']()} *${(new Date - user.afk).toTimeString()}*`.trim())
+await conn.reply(m.chat, `${lenguajeGB['smsAvisoEG']()}✴️ *A F K* ✴️
+*▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔*
+*@${m.sender.split("@")[0]}* ${lenguajeGB['smsAfkM1']()}${user.afkReason ? `\n${lenguajeGB['smsAfkM2']()}👉 ` + user.afkReason : ''}
+
+${lenguajeGB['smsAfkM3']()}\n👉 *${(new Date - user.afk).toTimeString()}*`.trim(), m, { mentions: [m.sender] })
 user.afk = -1
 user.afkReason = ''
 }
@@ -17,10 +19,12 @@ let afkTime = user.afk
 if (!afkTime || afkTime < 0)
 continue
 let reason = user.afkReason || ''
-m.reply(`${lenguajeGB['smsAfkM4']()}     
-${reason ? `${lenguajeGB['smsAfkM5']()} ` + reason : `${lenguajeGB['smsAfkM6']()}`}
-${lenguajeGB['smsAfkM3']()} *${(new Date - afkTime).toTimeString()}*
-`.trim())
+await conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}✴️ *A F K* ✴️
+*▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔*
+😾 ${lenguajeGB['smsAfkM4']()}\n${reason ? `${lenguajeGB['smsAfkM5']()}` + '👉 ' + reason : `${lenguajeGB['smsAfkM6']()}`}
+
+${lenguajeGB['smsAfkM3']()}\n👉 *${(new Date - user.afk).toTimeString()}*`.trim(), m)
 }
 return true
 }
+export default handler
