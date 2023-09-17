6fbe81d40e22a3df220ad33145d5fdf6834dd4bc
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-09-17 09:43:27 -0500

    𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂

--- plugins/_antispam_.js
+++ plugins/_antispam_.js
@@ -1,21 +1,73 @@
-//CRÉDITOS: https://github.com/Abiguelreyes75
+const userSpamData = {}
+let handler = m => m
+handler.before = async function (m, { conn }) {
+    let user = global.db.data.users[m.sender]
+    const sender = m.sender
+    const currentTime = new Date().getTime()
+    const timeWindow = 4000  
+    const messageLimit = 4  
 
-export async function all(m, conn) { 
-let texto
-let user = global.db.data.users[m.sender]  
-if (!m.message)
-return
-if (!user.antispam)
-return !0
-if (+new Date() > user.antispam) {
-let tiempo = 60000 * 1
-setTimeout(() => {
-//this.updateBlockStatus(m.chat, 'unblock') No me gustó jjjjj
-user.banned = false
-texto = `*@${m.sender.split("@")[0]} ${lenguajeGB['smsNoSpam2']()}*`
-conn.reply(m.chat, texto, m, { mentions: this.parseMention(texto) })}, tiempo) 
-//this.sendButton(m.chat, texto, wm, null, [[lenguajeGB.smsConMenu(), '/menu']], m, { mentions: this.parseMention(texto) })}, tiempo)        
-user.antispam = null
-}}
+    if (!(sender in userSpamData)) {
+        userSpamData[sender] = {
+            lastMessageTime: currentTime,
+            messageCount: 1,
+            antiBan: 0, 
+        }
+    } else {
+        const userData = userSpamData[sender]
+        const timeDifference = currentTime - userData.lastMessageTime
 
- 
+if (userData.antiBan === 1) {
+conn.reply(m.chat, `@${sender.split("@")[0]} No puede hacer nada por 30 segundos`, m)
+} else if (userData.antiBan === 2) {
+conn.reply(m.chat, `@${sender.split("@")[0]} No puede hacer nada por 1 minuto`, m)
+} else if (userData.antiBan === 3) {
+conn.reply(m.chat, `@${sender.split("@")[0]} No puede hacer nada por 2 minutos`, m)
+}
+
+        if (timeDifference <= timeWindow) {
+            userData.messageCount += 1
+
+            if (userData.messageCount >= messageLimit) {
+                const mention = `@${sender.split("@")[0]}`
+                const warningMessage = `Baneado ${mention} por enviar spam.`
+                conn.reply(m.chat, warningMessage, m)
+                user.banned = true
+                userData.antiBan++
+                userData.messageCount = 1
+                
+                
+                if (userData.antiBan === 1) {
+                    setTimeout(() => {
+                        if (userData.antiBan === 1) {
+                            userData.antiBan = 0
+                            user.banned = false
+                        }
+                    }, 30000) // 30 segundos
+                } else if (userData.antiBan === 2) {
+                    setTimeout(() => {
+                        if (userData.antiBan === 2) {
+                            userData.antiBan = 0
+                            user.banned = false
+                        }
+                    }, 60000) // 1 minuto
+                } else if (userData.antiBan === 3) {
+                    setTimeout(() => {
+                        if (userData.antiBan === 3) {
+                            userData.antiBan = 0
+                            user.banned = false
+                        }
+                    }, 120000) // 2 minutos
+                }
+            }
+        } else {
+            if (timeDifference >= 2000) {
+                userData.messageCount = 1
+            }
+        }
+
+        userData.lastMessageTime = currentTime
+    }
+}
+
+export default handler
