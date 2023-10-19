7e8c315307241e4cb7fe1dd297512f03aa11a53e
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-10-18 21:31:26 -0500

    卐 𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂 卐

--- plugins/descargas-tiktokfotos.js
+++ plugins/descargas-tiktokfotos.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch' 
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 if (!text) throw `*⚠️ INGRESE UN ENLACE DE TIKTOK QUE CONTENGA IMAGENES.*`
