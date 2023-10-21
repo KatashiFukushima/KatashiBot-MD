abe1c10b4164fcb94bde58aac0cdeaf405de27d1
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-10-21 13:39:15 -0600

    New text

--- plugins/info-firstchat.js
+++ plugins/info-firstchat.js
@@ -5,12 +5,10 @@ if (m.chat.endsWith('broadcast') || m.fromMe || m.isGroup) return
 let user = global.db.data.users[m.sender]
 
 if (new Date() - user.pc < 1) return
-await m.reply(`👋 Hola!!
+await m.reply(`🌟 *Bienvenido a KatashiBot* 🥷🏻
+📲 _Ejecute el comando .menu para visualizar los comandos que dispongo_ 🫡
 
-⚠️ Nota: no envíe spam al bot
-🧃 Escriba .menu para mostrar el menú 
-  
-📝 ¿Quieres apoyar este proyecto para que siga actualizándose? enviar a través de: 
-*https://paypal.me/azami.19*`) 
+📌 *Cualquier duda o sugerencia puede contactar a mi creador:* 🎉
+📩 wa.me/51948705559 📭`) 
 user.pc = new Date * 1
 }
