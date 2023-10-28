0754c828eee82ed26988e26d0bf91d1982c76e4a
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-10-28 11:37:46 -0500

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -4,7 +4,7 @@ import uploadImage from '../lib/uploadImage.js'
 import { webp2png } from '../lib/webp2mp4.js'
 
 let handler = async (m, { conn, args, usedPrefix, command, text }) => {
-const fkontak = {"key": {"participants": "0@s.whatsapp.net", "remoteJid": "0@s.whatsapp.net" }, message: { groupInviteMessage: { groupJid: "6288213840883-1616169743@g.us", sourceUrl: n2, mediaType: 2, groupName: "AzamiJs", thumbnail: imagen1`}}}
+const fkontak = {"key": {"participants": "0@s.whatsapp.net", "remoteJid": "0@s.whatsapp.net" }, message: { groupInviteMessage: { groupJid: "6288213840883-1616169743@g.us", sourceUrl: n2, mediaType: 2, title: wm, thumbnail: imagen1`}}}
 let pp = 'https://c4.wallpaperflare.com/wallpaper/991/456/22/sketch-artist-anime-anime-girls-arknights-swire-arknights-hd-wallpaper-preview.jpg'
 let stiker = false
 let q = m.quoted ? m.quoted : m
