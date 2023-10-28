f5982b98473fefc4c154e84a9a0fd0307ca3ef56
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-10-28 12:33:08 -0500

    卐 𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂 卐

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -153,7 +153,7 @@ conn.sendHydrated(m.chat, info, wm, pp, 'https://github.com/GataNina-Li/GataBot-
 }
 handler.help = ['infobot']
 handler.tags = ['info', 'tools']
-handler.command = /^(infobot|informacionbot|infogata|informacióngata|informaciongata)$/i
+handler.command = /^(infobot|informacionbot|infokata|informaciónkata|informacionkata)$/i
 export default handler
 
 function clockString(ms) {
