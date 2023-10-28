1eda728b724d21d6e58e6d5967228be8aac6ba65
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-10-28 13:25:11 -0500

    卐 𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂 卐

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 # Código desarrollado por @gata_dios 
   
 #!/data/data/com.termux/files/usr/bin/bash 
-BOT_DIR="GataBot-MD"  
-BOT_REPO="https://github.com/GataNina-Li/$BOT_DIR"
+BOT_DIR="KatashiBot-MD"  
+BOT_REPO="https://github.com/KatashiFukushima/$BOT_DIR"
 DB_FILE="database.json"
  
 GREEN='\033[32m'
