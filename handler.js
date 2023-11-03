ae9af70dcc5432cb53dd909abdf50206b47f2922
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-11-03 16:16:05 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -1099,7 +1099,7 @@ if (plugin.tags && plugin.tags.includes('admin')) {
 // global.dfail('restrict', m, this)
 continue
 }
-const str2Regex = str => str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
+const str2Regex = str => str.replace(/[|\m\{}()[\]^$+*?.]/g, '\\$&')
 let _prefix = plugin.customPrefix ? plugin.customPrefix : conn.prefix ? conn.prefix : global.prefix
 let match = (_prefix instanceof RegExp ? // RegExp Mode?
 [[_prefix.exec(m.text), _prefix]] :
