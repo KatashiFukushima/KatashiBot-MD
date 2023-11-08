a10ad8fa3e2018b15bc7995048a0dd39091b6052
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-11-07 19:33:18 -0500

    𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const require = createRequire(__dirname);
 const { name, author } = require(join(__dirname, "./package.json"));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-
+  
 say('Katashi\nBot\nMD', {
 font: 'chrome',
 align: 'center',
