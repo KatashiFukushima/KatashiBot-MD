44614ea3e8be4ebcf50d494f0c21f2107edf7ae2
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-12-04 08:03:50 -0500

    𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂

--- katashi.sh
+++ katashi.sh
@@ -335,4 +335,4 @@ _____░▒▓██\n\e[0m"
     
 sleep 15 && pkill mpv > /dev/null 2>&1 && sleep 2 && rm -f /data/data/com.termux/files/home/"$random_mp3" &
 echo -e "\033[01;32m\033[01mIniciando KatashiBot!!\nStarting KatashiBot!!\n\033[0m"
-npm start
+npm run qr
