bed00850a97d3ef72c8acbe63d28c08dcad56f6c
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-12-08 23:46:36 -0500

    𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -29,7 +29,11 @@ let str = `💕 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝙊𝙎
 
 *Por favor, no ingresar con números de Bots, y mantener el respeto.*\n
 *Please, do not enter with Bot numbers, and maintain respect.*`
-await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
+//await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
+const vi = ['https://telegra.ph/file/7f0318607ccfdb6bcb66d.mp4',
+'https://telegra.ph/file/a848eeb479e662f2e3fab.mp4',
+'https://telegra.ph/file/117391db9016a51f73618.mp4']
+await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: str, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak })
 /*conn.sendHydrated(m.chat, str, `𝙂𝘼𝙏𝘼 𝘿𝙄𝙊𝙎 - 𝘼𝙎𝙄𝙎𝙏𝙀𝙉𝘾𝙄𝘼\n${asistencia}\n\n` + wm, media, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
 ['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', '.cuentasgb'],
 ['🎁 𝘿𝙤𝙣𝙖𝙧 | 𝘿𝙤𝙣𝙖𝙩𝙚', '.donar'],
