6fb8371fe3445d832748acbfb42f396968569cdf
WilsonWaoz <135527239+WilsonOFC@users.noreply.github.com>
2023-12-14 11:17:24 -0300

    𝙆𝙖𝙩𝙖𝙨𝙝𝙞 𝙁𝙪𝙠𝙪𝙨𝙝𝙞𝙢𝙖

--- plugins/stickers-qc.js
+++ plugins/stickers-qc.js
@@ -13,7 +13,7 @@ let text
     } else if (m.quoted && m.quoted.text) {
         text = m.quoted.text
     } else throw "╰⊱❗️⊱ *𝙇𝙊 𝙐𝙎𝙊́ 𝙈𝘼𝙇 | 𝙐𝙎𝙀𝘿 𝙄𝙏 𝙒𝙍𝙊𝙉𝙂* ⊱❗️⊱╮\n\n𝘼𝙂𝙍𝙀𝙂𝙐𝙀́ 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊 𝙋𝘼𝙍𝘼 𝘾𝙍𝙀𝘼𝙍 𝙀𝙇 𝙎𝙏𝙄𝘾𝙆𝙀𝙍\n\n𝘼𝘿𝘿 𝘼 𝙏𝙀𝙓𝙏 𝙏𝙊 𝘾𝙍𝙀𝘼𝙏𝙀 𝙏𝙃𝙀 𝙎𝙏𝙄𝘾𝙆𝙀𝙍 "
-   if (!text) return m.reply('𝙔 𝙀𝙇 𝙏𝙀𝙓𝙏𝙊?')
+   if (!text) return m.reply('𝘿𝙤𝙣𝙙𝙚 𝙘𝙖𝙧𝙖𝙟𝙤𝙨 𝙚𝙨t𝙖 𝙚𝙡 𝙩𝙚𝙭𝙩𝙤?')
    if (text.length > 30) return m.reply('𝙈𝘼𝙓𝙄𝙈𝙊 30 𝙋𝘼𝙇𝘼𝘽𝙍𝘼𝙎!')
     let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/a2ae6cbfa40f6eeea0cf1.jpg')
 
