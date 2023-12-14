fbad855dbee4f393ef4a7f5a572d28e20fd2d2b6
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-12-14 00:38:02 -0500

    𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂

---
+++ plugins/herramientas-bard.js
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch'
+
+var handler = async (m, { text,  usedPrefix, command }) => {
+
+if (!text) throw `*🤨 ¿Qué carajo quiere buscar? 🤬 ¡Ingrese un texto!* `
+
+try {
+
+await m.reply('🌐 *_Aguarde un momento..._*')
+var apii = await fetch(`https://api.botcahx.live/api/search/bard-ai?apikey=97Fxy6mj&text=${text}`)
+var res = await apii.json()
+await m.reply(res.message)
+
+} catch (error) {
+console.error(error)
+throw '⚠️ *CARAJO, HA OCURRIDO UN ERROR* 😳'
+}
+
+}
+handler.command = ['bard']
+handler.help = ['bard']
+handler.tags = ['ai']
+
+handler.premium = false
+
+export default handler
