443ac9d88f60bbe9f860b5e4ab74d16b04390b63
SoIz1 <136395166+SoIz1@users.noreply.github.com>
2024-03-05 19:44:56 -0600

    Update rpg-rob.js

--- plugins/rpg-rob.js
+++ plugins/rpg-rob.js
@@ -12,9 +12,9 @@ let rob = Math.floor(Math.random() * ro)
 if (users.exp < rob) return m.reply(`😿 @${who.split`@`[0]} tiene menos de *${ro} XP* No robes a un pobre :v`, null, { mentions: [who] })    
 global.db.data.users[m.sender].exp += rob
 global.db.data.users[who].exp -= rob 
-global.db.data.users[m.sender].money += rob
-global.db.data.users[who].money -= rob 
-m.reply(`*✧ Robaste ${rob} Xp a @${who.split`@`[0]}*`, null, { mentions: [who] })
+global.db.data.users[m.sender].money += rob2
+global.db.data.users[who].money -= rob2 
+m.reply(`*✧ Robaste ${rob} Xp y ${rob2} coins a @${who.split`@`[0]}*`, null, { mentions: [who] })
 global.db.data.users[m.sender].lastrob = new Date * 1
 }
 handler.help = ['rob']
