7c154833e219e2f6f316368823573648a870c9da
SoIz1 <136395166+SoIz1@users.noreply.github.com>
2024-03-09 22:22:56 -0600

    Create onlycodetest.js

---
+++ plugins/onlycodetest.js
@@ -0,0 +1,19 @@
+function generarCodigo() {
+const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+const numeros = '0123456789'
+const caracteresEspeciales = '#@%_*!^<'
+let codigo = ''
+for (let i = 0; i < 3; i++) {
+codigo += letras.charAt(Math.floor(Math.random() * letras.length))
+}
+for (let i = 0; i < 2; i++) {
+codigo += numeros.charAt(Math.floor(Math.random() * numeros.length))
+}
+for (let i = 0; i < 1; i++) {
+codigo += caracteresEspeciales.charAt(Math.floor(Math.random() * caracteresEspeciales.length))
+}
+codigo = codigo.split('').sort(() => Math.random() - 0.5).join('');
+return codigo
+}
+handler.command = /^rcode$/i
+export default handler
