29897c11df0260c1cfa0d57ff4879ab0b4228767
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2024-05-20 23:04:53 -0500

    𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂

--- plugins/info-cuentas-oficiales.js
+++ plugins/info-cuentas-oficiales.js
@@ -19,12 +19,8 @@ let str = `💙 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝘼𝙎
 *Si tienen dudas, sugerencias, o preguntas solo escriban por Instagram.*\n
 *If you have doubts, suggestions or questions just write on Instagram.*`
 await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
-/*conn.sendHydrated(m.chat, str, wm, media, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
-['𝙂𝙧𝙪𝙥𝙤𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝙂𝙧𝙤𝙪𝙥𝙨 🔰', '.grupos'],
-['𝘾𝙧𝙚𝙖𝙙𝙤𝙧𝙖 | 𝘾𝙧𝙚𝙖𝙩𝙤𝙧 💗', '#owner'],
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
-], m,)}*/
 
-handler.command = /^cuentasoficiales|gataig|cuentaskb|cuentagb|accounts|gataaccounts|account|iggata|cuentasdegata|cuentasdegatabot|cuentagatabot|cuentaskatashibot$/i
+
+handler.command = /^cuentasoficiales|gataig|cuentaskb|cuentakb|accounts|katashiaccounts|account|igkatashi|cuentasdekatashi|cuentasdekatashibot|cuentakatashibot|cuentaskatashibot$/i
 handler.exp = 35
 export default handler
