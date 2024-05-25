983dc848bca6ece67b758f5673bc3a26a3169b78
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2024-05-25 18:23:19 -0500

    𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂

--- plugins/grupo-hidetag.js
+++ plugins/grupo-hidetag.js
@@ -38,5 +38,5 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
 };
 handler.command = /^(hidetag|notificar|notify)$/i
 handler.group = true
-handler.admin = false
+handler.admin = true
 export default handler
