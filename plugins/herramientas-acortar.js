ff272d94a4e6349b0e6716fde1795008336db0a1
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2024-06-04 01:20:53 -0500

    𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂

---
+++ plugins/herramientas-acortar.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, xteamkey }) => {
+if (!text) throw `${mg}${mid.smsAcorta}`
+try {
+let json = await (await fetch(`https://api.xteam.xyz/shorturl/tinyurl?url=${text}&apikey=cb15ed422c71a2fb`)).json()
+if (!json.status) throw json
+let hasil = `${smsAcorta(text)}\n*${json.result}*`.trim()   
+m.reply(hasil)
+} catch (e) {
+await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, fkontak, m)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)}}
+handler.help = ['tinyurl','acortar'].map(v => v + ' <link>')
+handler.tags = ['tools']
+handler.command = /^(tinyurl|short|acortar|corto)$/i
+handler.fail = null
+export default handler
---
+++ plugins/herramientas-chatGPT.js
@@ -0,0 +1,24 @@
+import fetch from 'node-fetch';
+import axios from 'axios';
+import translate from '@vitalets/google-translate-api';
+import {Configuration, OpenAIApi} from 'openai';
+const configuration = new Configuration({organization: global.openai_org_id, apiKey: global.openai_key});
+const openaiii = new OpenAIApi(configuration);
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+if (usedPrefix == 'a' || usedPrefix == 'A') return;
+if (!text) throw `*${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉𝘼 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉 𝙊 𝙐𝙉𝘼 𝙊𝙍𝘿𝙀𝙉 𝙋𝘼𝙍𝘼 𝙐𝙎𝘼𝙍 𝙇𝘼 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝘾𝙃𝘼𝙏𝙂𝙋𝙏\n\n❏ 𝙀𝙅𝙀𝙈𝙋𝙇𝙊 𝘿𝙀 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉𝙀𝙎 𝙔 𝙊𝙍𝘿𝙀𝙉𝙀𝙎\n❏ ${usedPrefix + command} Recomienda un top 10 de películas de acción\n❏ ${usedPrefix + command} Codigo en JS para un juego de cartas`    
+try {
+conn.sendPresenceUpdate('composing', m.chat);
+let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/ia2?text=${text}`)
+let res = await gpt.json()
+await m.reply(res.gpt)
+} catch {
+try {
+let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/chatgpt?q=${text}`)
+let res = await gpt.json()
+await m.reply(res.data)
+} catch {
+}}}
+handler.command = /^(openai|chatgpt|ia|ai|openai2|chatgpt2|ia2)$/i;
+export default handler;
+
