dce2967ddeab557ae026dd78837d02e307ab5733
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2024-06-06 23:49:17 -0500

    𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂

--- plugins/rpg-transfer.js
+++ plugins/rpg-transfer.js
@@ -20,7 +20,7 @@ async function handler(m, { conn, args, usedPrefix, command }) {
 ┃ ✅ *RECURSOS DISPONIBLES*
 ┃──────────────
 ┃ limit *= Diamantes* 💎
-┃ money *= GataCoins* 🐈
+┃ money *= KataCoins* 🪙
 ┃ exp *= Experiencia* ⚡
 ╰━━━━━━━━━━━━━━━━━━ ღ 
 `.trim()
