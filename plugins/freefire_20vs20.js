7f70f3f9399d7c1bbcdb4974543b03df47102935
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-06-09 03:01:03 -0500

    Create freefire_20vs20.js

---
+++ plugins/freefire_20vs20.js
@@ -0,0 +1,75 @@
+let handler = async (m, { conn, command, usedPrefix }) => {
+let ff = `https://telegra.ph/file/0ea56fa1fba7bdbb619ae.jpg`
+let vs20txt = `
+╭──────⚔──────╮
+ㅤㅤ20 𝐕𝐄𝐑𝐒𝐔𝐒 20
+╰──────⚔──────╯
+╭──────────────╮
+│ㅤ⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎 
+│ㅤ🇲🇽 𝐌𝐄𝐗 : 
+│ㅤ🇨🇴 𝐂𝐎𝐋 : 
+│➥ 𝐂𝐎𝐋𝐎𝐑 𝐃𝐄 𝐑𝐎𝐏𝐀: 
+│➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:
+│
+│     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 1
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│     
+│    𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 2
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│
+│    𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 3
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│
+│    𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 4
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│
+│    𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 5
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│
+│ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
+│⚜️ ➤ 
+│⚜️ ➤ 
+╰─────────────╯
+                 
+` //Aquí arriba cambiar el texto sin borrar las comillas
+await conn.sendFile(m.chat, 'https://telegra.ph/file/0ea56fa1fba7bdbb619ae.jpg', 'fantasy.jpg', vs20txt.trim(),
+//En esta imagen vas a poner el url de la imagen grande
+ fkontak, true, {
+contextInfo: {
+'forwardingScore': 200,
+'isForwarded': false,
+externalAdReply: {
+showAdAttribution: true,
+renderLargerThumbnail: false,
+title: `Free Fire`,
+body: `vs`,
+mediaType: 1,
+sourceUrl: 'https://www.instagram.com/ricardoookadm_',
+thumbnailUrl: 'https://telegra.ph/file/0ea56fa1fba7bdbb619ae.jpg'
+//Aquí arriba vas a poner el url de la imagen que irá junto a tu link de tu red social.
+}}
+}, { mentions: m.sender })
+
+}
+handler.command = /^(20vs20|vs20)$/i
+export default handler
