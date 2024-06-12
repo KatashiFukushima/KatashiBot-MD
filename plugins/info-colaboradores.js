05554888c5fb1c2c16bdf7c950739bbc9b6811f1
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2024-06-12 14:12:05 -0500

    Update info-colaboradores.js

--- plugins/info-colaboradores.js
+++ plugins/info-colaboradores.js
@@ -1,42 +1,47 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
 let ff = `https://qu.ax/ZnYx.jpeg`
-let vs12txt = `
-╭──────⚔──────╮
-ㅤㅤ12 𝐕𝐄𝐑𝐒𝐔𝐒 12
-╰──────⚔──────╯
-╭──────────────╮
-│ㅤ⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎 
-│ㅤ🇲🇽 𝐌𝐄𝐗 : 
-│ㅤ🇨🇴 𝐂𝐎𝐋 : 
-│➥ 𝐂𝐎𝐋𝐎𝐑 𝐃𝐄 𝐑𝐎𝐏𝐀: 
-│➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:
-│
-│     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 1
-│
-│👑 ➤ 
-│⚜️ ➤ 
-│⚜️ ➤ 
-│⚜️ ➤ 
-│     
-│    𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 2
-│
-│👑 ➤ 
-│⚜️ ➤ 
-│⚜️ ➤ 
-│⚜️ ➤ 
-│
-│    𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 3
-│
-│👑 ➤ 
-│⚜️ ➤ 
-│⚜️ ➤ 
-│⚜️ ➤ 
-│
-│ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
-│⚜️ ➤ 
-│⚜️ ➤ 
-╰─────────────╯
-` //Aquí arriba cambiar el texto sin borrar las comillas
+let vs12txt = `💫 *EQUIPO STAFF*
+🥷 *Bot:* ${global.botname}
+☁️ *Versión:* ${global.vs}
+
+👑 *Propietario:*
+
+• Diego-YL-177
+☘️ *Rol:* Propietario
+✨️ *Número:* ${creador}
+🏆 *GitHub:* https://github.com/Diego-YL-177
+
+💫 *Colaboradores:*
+
+• GataNina-Li
+☘️ *Rol:* Developer
+✨️ *Número:* Wa.me/593968263524
+🏆 *GitHub:* https://github.com/GataNina-Li
+
+• elrebelde21
+☘️ *Rol:* Developer
+✨️ *Número:* Wa.me/573147616444
+🏆 *GitHub:* https://github.com/elrebelde21
+
+• KatashiFukushima
+☘️ *Rol:* Developer
+✨️ *Número:* Wa.me/51948705559
+🏆 *GitHub:* https://github.com/KatashiFukushima
+
+• AzamiJs
+☘️ *Rol:* Developer
+✨️ *Número:* Wa.me/5214434703586
+🏆 *GitHub:* https://github.com/AzamiJs
+
+• Alba070503
+☘️ *Rol:* Developer
+✨️ *Número:* Wa.me/59169082575
+🏆 *GitHub:* https://github.com/Alba070503
+
+• Danixl.Js
+☘️ *Rol:* Developer
+✨️ *Número:* Wa.me/595983799436
+🏆 *GitHub:* https://github.com/DanixlJs ` //Aquí arriba cambiar el texto sin borrar las comillas
 await conn.sendFile(m.chat, 'https://qu.ax/ZnYx.jpeg', 'fantasy.jpg', vs12txt.trim(),
 //En esta imagen vas a poner el url de la imagen grande
  fkontak, true, {
