2423678c03d74e3885e9a14e6448b23c5973dfb0
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2024-06-12 23:45:15 -0500

    𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -606,9 +606,9 @@ ${generateCommand(commandsConfig, usedPrefix).replace(/≡/g, '𖡡')}
 `.trim()
 //await conn.sendFile(m.chat, gataImg, 'lp.jpg', menu, fkontak, false, { contextInfo: {mentionedJid, externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' 🥷 𝗦𝘂𝗽𝗲𝗿 𝗞𝗮𝘁𝗮𝘀𝗵𝗶𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', previewType: 0, thumbnail: imagen4, sourceUrl: redesMenu }}})
 //conn.sendFile(m.chat, gataVidMenu.getRandom(), 'gata.mp4', menu, fkontak)
-const vi = ['https://qu.ax/dxnH.mp4',
-'https://qu.ax/dxnH.mp4',
-'https://qu.ax/dxnH.mp4']
+const vi = ['https://qu.ax/TNPH.mp4',
+'https://qu.ax/srFl.mp4',
+'https://qu.ax/yLtv.mp4']
 await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak })
  
 } catch (e) {
