cb3926e001603440228e295743977cc7f37e74f5
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2024-06-16 00:50:04 -0500

    Update _a-nada.js

--- plugins/_a-nada.js
+++ plugins/_a-nada.js
@@ -1,9 +1,4 @@
-let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, command }) => {
-let chat = global.db.data.chats[m.chat]
-let user = global.db.data.users[m.sender]
-let bot = global.db.data.settings[conn.user.jid] || {}
-
-  let handler = async (m, { conn, usedPrefix, command}) => {
+let handler = async (m, { conn, usedPrefix, command}) => {
 let pp = ['https://qu.ax/Tvpv.mp4', 'https://qu.ax/Tvpv.mp4','https://qu.ax/Tvpv.mp4']
 //let pp2 = 'https://tinyurl.com/294oahv9'
 let who
@@ -13,10 +8,9 @@ if (!who) throw '✦ Menciona al usuario con *@user*'
 let name2 = conn.getName(who)
 let name = conn.getName(m.sender)
 
-
 await conn.sendMessage(m.chat, { video: { url: pp.getRandom() }, gifPlayback: true, caption: `*${name}*` + ' está saludando a' + ` *${name2}*` + ' (ﾉ^ヮ^)ﾉ*:・ﾟ✧', contextInfo: fakeChannel }, { quoted: m })
 }
-handler.help = ['dance <@user>']
+handler.help = ['saludar <@user>']
 handler.tags = ['fun']
-handler.command = ['hola', 'saludar']
+handler.command = ['saludar', 'hola']
 export default handler
