dd4f17727963be5e02df42c1ea9fb81cb216fc9d
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2024-06-16 00:41:48 -0500

    Create _a-nada.js

---
+++ plugins/_a-nada.js
@@ -0,0 +1,22 @@
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, command }) => {
+let chat = global.db.data.chats[m.chat]
+let user = global.db.data.users[m.sender]
+let bot = global.db.data.settings[conn.user.jid] || {}
+
+  let handler = async (m, { conn, usedPrefix, command}) => {
+let pp = ['https://media.tenor.com/FJzcVnWgHjgAAAAM/wave.gif', 'https://media.tenor.com/FJzcVnWgHjgAAAAM/wave.gif','https://media.tenor.com/FJzcVnWgHjgAAAAM/wave.gif']
+//let pp2 = 'https://tinyurl.com/294oahv9'
+let who
+if (m.isGroup) who = m.mentionedJid[0]
+else who = m.chat
+if (!who) throw '✦ Menciona al usuario con *@user*'
+let name2 = conn.getName(who)
+let name = conn.getName(m.sender)
+
+
+await conn.sendMessage(m.chat, { video: { url: pp.getRandom() }, gifPlayback: true, caption: `*${name}*` + ' está saludando a' + ` *${name2}*` + ' (ﾉ^ヮ^)ﾉ*:・ﾟ✧', contextInfo: fakeChannel }, { quoted: m })
+}
+handler.help = ['dance <@user>']
+handler.tags = ['fun']
+handler.command = ['hola', 'saludar']
+export default handler
