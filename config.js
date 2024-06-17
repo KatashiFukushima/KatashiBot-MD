ed0d9ff3854d18f8d145f177ae0599383e6a6978
SoIz1 <136395166+SoIz1@users.noreply.github.com>
2024-06-17 16:05:14 -0600

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
 ['56974513824', '☆𝑲𝒂𝒕𝒂𝒔𝒉𝒊-𝑩𝒐𝒕☆', false],
-['51948705559', '𝙊𝙬𝙣𝙚𝙧', true],  
+['51948705559', '𝙊𝙬𝙣𝙚𝙧', false],  
 ['5217442363122', '𝑖𝑍𝑖', true],
 ['59175655964', 'Alba070503', false],
 ['5217294888993'], ['5217445404792'],
