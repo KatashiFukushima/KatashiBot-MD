df4be04c26b4459aaf7c599bf2811a9e342a5b1f
SoIz1 <136395166+SoIz1@users.noreply.github.com>
2024-06-17 16:08:49 -0600

    Create _test.js

---
+++ plugins/_test.js
@@ -0,0 +1,31 @@
+let axios = require("axios");
+let handler = async(m, { conn, text }) => {
+
+   await m.reply('Searching...')
+    if (!text) return conn.reply(m.chat, 'Masukan Alamat IP yang akan dicek', m)
+
+	axios.get(`https://videfikri.com/api/iplookup/?ip=${text}`).then ((res) => {
+
+let hasil = `
+*IP CHECKER*
+
+IP : ${res.data.result.ip}
+Negara : ${res.data.result.country}
+Kode Negara : ${res.data.result.country_code}
+Provinsi : ${res.data.result.region_name}
+Kode Provinsi : ${res.data.result.region}
+Kota : ${res.data.result.city}
+Kordinat :
+${res.data.result.latitude}, ${res.data.result.longtitude}
+Zona Waktu : ${res.data.result.timezone}
+ISP : ${res.data.result.isp}
+AS : ${res.data.result.as}
+`.trim()
+
+    conn.reply(m.chat, hasil, m)
+	})
+}
+handler.help = ['ip', 'ipcheck', 'ipcek'].map(v => v + ' <alamat ip>')
+handler.tags = ['tools']
+handler.command = /^(ip|ipcheck|ipcek)$/i
+handler.owner = false
