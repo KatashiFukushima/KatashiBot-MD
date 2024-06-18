6c902c64541a49e8f526dafb1f43f0fab1da38cd
SoIz1 <136395166+SoIz1@users.noreply.github.com>
2024-06-17 20:11:58 -0600

    testIP

--- plugins/_test.js
+++ plugins/_test.js
@@ -4,28 +4,28 @@ let handler = async (m, { conn, text }) => {
 await m.reply("Buscando...");
   if (!text) return conn.reply(m.chat, "Ingrese una dirección IP válida", m);
 
-  axios.get(`http://ip-api.com/json/${text}?fields=status,message,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,isp,org,as,mobile,hosting,query`).then 
+  axios.get(`http://ip-api.com/json/${text}?fields=status,message,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,isp,org,as,mobile,hosting,query`).then ((res) => {
     
     let hasil = `
 *IP CHECKER*
 
-IP : ${data.query}
-País : ${data.country}
-Código de País : ${data.countryCode}
-Provincia : ${data.regionName}
-Código de Provincia : ${data.region}
-Ciudad : ${data.city}
-Distrito : ${data.district}
-Código Postal : ${data.zip}
-Coordenadas : ${data.lat}, ${data.lon}
-Zona Horaria : ${data.timezone}
-ISP : ${data.isp}
-Organización : ${data.org}
-AS : ${data.as}
-Mobile : ${data.mobile ? "Si" : "No"}
-Hosting : ${data.hosting ? "Si" : "No"}
+IP : ${res.data.query}
+País : ${res.data.country}
+Código de País : ${res.data.countryCode}
+Provincia : ${res.data.regionName}
+Código de Provincia : ${res.data.region}
+Ciudad : ${res.data.city}
+Distrito : ${res.data.district}
+Código Postal : ${res.res.data.zip}
+Coordenadas : ${res.data.lat}, ${data.lon}
+Zona Horaria : ${res.data.timezone}
+ISP : ${res.data.isp}
+Organización : ${res.data.org}
+AS : ${res.data.as}
+Mobile : ${res.data.mobile ? "Si" : "No"}
+Hosting : ${res.data.hosting ? "Si" : "No"}
 `.trim();
-conn.reply(m.chat, hasil, m)}
+conn.reply(m.chat, hasil, m)})
     
 handler.help = ['ip', 'ipcheck', 'ipcek'].map(v => v + ' <alamat ip>')
 handler.tags = ['tools']
