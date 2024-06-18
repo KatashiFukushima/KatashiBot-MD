38a5501a6ef4fa6f9fc1b652d974a6a0ebbe8303
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2024-06-17 23:00:35 -0500

    𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂

--- plugins/_content.js
+++ plugins/_content.js
@@ -8,58 +8,14 @@ var handler = m => m
 handler.all = async function (m) {
 global.key = ''
 
-// Cuentas
-const response = await fetch('https://raw.githubusercontent.com/KatashiFukushima/KatashiBot-MD/master/official_accounts.json')  
-const data = await response.json()
-let { accounts, channels, groups, collaboration, sponsors, others } = data.info
-
-global.yt = accounts.youTube
-global.yt2 = others.yt_vid
-global.ig = accounts.instagram
-global.md = accounts.gatabot_md
-global.fb = accounts.facebook
-global.tk = accounts.tiktok
-global.ths = accounts.threads
-global.paypal = accounts.paypal
-global.asistencia = others.assistance_num
-global.bot = 'wa.me/51984034663'
-global.cuentas = accounts.all
-
-global.canal1 = channels.channel1
-global.canal2 = channels.channel2
-global.canal3 = channels.channel3
-global.canal4 = channels.channel4
-
-global.soporteGB = others.group_support
-global.grupo1 = groups.group1
-global.grupo2 = groups.group2
-global.grupo3 = groups.group3
-global.grupo4 = groups.group4
-global.grupo5 = groups.group5
-global.grupo6 = groups.group6
-
-global.grupo_collab1 = collaboration.group1
-global.grupo_collab2 = collaboration.group2
-global.grupo_collab3 = collaboration.group3
-global.grupo_collab4 = collaboration.group4
-
-global.patrocinador1 = sponsors.boxmine
-global.patrocinador2 = sponsors.cafirexos
-global.patrocinador3 = sponsors.vortexus
-global.patrocinador4 = sponsors.asif
-
 global.canales = [canal1, canal2, canal3, canal4].getRandom()
 global.welgata = [tk, ig, yt2, yt2, ig, md, ig, yt, paypal, yt2, yt2, ig, fb, tk, ths, asistencia].getRandom()
 global.redesMenu = [canal1, canal2, canal3, canal4, soporteGB, grupo1, grupo2, grupo3, grupo4, grupo5, grupo6, md, ig, paypal, yt, asistencia, fb, tk].getRandom()
 global.accountsgb = [canal1, canal2, canal3, canal4, tk, ig, yt, paypal, fb, ths, md, asistencia].getRandom()
 
-var canalesInfo = [
-{ link: canal1, id: "120363178367665133@newsletter", name: "💫👁️‍🗨️ Katashi Fukushima 🥷🏻🚀" },
-{ link: canal2, id: "120363178367665133@newsletter", name: "💫👁️‍🗨️ Katashi Fukushima 🥷🏻🚀" },
-{ link: canal3, id: "120363178367665133@newsletter", name: "💫👁️‍🗨️ Katashi Fukushima 🥷🏻🚀" } 
-]
-var indiceAleatorio = Math.floor(Math.random() * canalesInfo.length)
-var channelRD = canalesInfo[indiceAleatorio]
+global.canalIdGB = ["120363178367665133@newsletter", "120363178367665133@newsletter", "120363178367665133@newsletter", "120363178367665133@newsletter"]
+global.canalNombreGB = ["💫👁️‍🗨️ Katashi Fukushima 🥷🏻🚀", "💫👁️‍🗨️ Katashi Fukushima 🥷🏻🚀", "💫👁️‍🗨️ Katashi Fukushima 🥷🏻🚀", "💫👁️‍🗨️ Katashi Fukushima 🥷🏻🚀"]
+global.channelRD = await getRandomChannel()
 
 // Imágenes 
 global.imagen1 = fs.readFileSync("./media/menus/Menu3.jpg")
@@ -94,7 +50,7 @@ global.img12 = 'https://telegra.ph/file/dc871409fc8c0a9ecab90.jpg'
 global.img13 = 'https://telegra.ph/file/ffdd4ed96c39c24abf89e.jpg'
 global.img14 = 'https://telegra.ph/file/65f5ccdcf2a879731894d.jpg'
 global.img15 = 'https://telegra.ph/file/d5314b1e4a1549cfd3158.jpg'
-  
+
 global.img16 = 'https://qu.ax/ovkN.jpg' //+18
 
 global.img17 = 'https://qu.ax/HJdr.jpg'
@@ -153,3 +109,10 @@ await conn.sendMessage(m.chat, { text: mensajes[i], edit: key })
 function pickRandom(list) {
 return list[Math.floor(Math.random() * list.length)]
   }
+
+async function getRandomChannel() {
+let randomIndex = Math.floor(Math.random() * canalIdGB.length)
+let id = canalIdGB[randomIndex]
+let nombre = canalNombreGB[randomIndex]
+return { id, nombre }
+} 	
