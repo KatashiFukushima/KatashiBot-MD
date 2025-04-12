33de75412322f6a08caacbf74adff67b51469694
SoIz1 <136395166+SoIz1@users.noreply.github.com>
2025-04-12 11:14:37 -0600

    Update propietario(a)-clearallsession.js

--- plugins/propietario(a)-clearallsession.js
+++ plugins/propietario(a)-clearallsession.js
@@ -32,7 +32,7 @@ const cleanSessionFiles = async (conn) => {
 
 setInterval(() => {
   cleanSessionFiles(global.conn);
-}, 3600000);
+}, 60000);
 
 const handler = async (m, { conn, usedPrefix }) => {
   if (global.conn.user.jid !== conn.user.jid) {
