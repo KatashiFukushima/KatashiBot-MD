59aa5f405b763fef3f0e884cfedd0b1a4b99631f
SoIz1 <136395166+SoIz1@users.noreply.github.com>
2025-07-23 18:05:49 -0600

    Update info-mc_server.js

--- plugins/info-mc_server.js
+++ plugins/info-mc_server.js
@@ -74,4 +74,4 @@ handler.help = ['mc <servidor:puerto> <bedrock|java>', 'minecraft <servidor> <ti
 handler.tags = ['games'];
 handler.limit = true; // Puedes ajustar esto
 
-export default mcHandler;
+export default handler;
